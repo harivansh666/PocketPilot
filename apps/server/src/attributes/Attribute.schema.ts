936c486efea22eeed23e8f3d7cbde23b8c84ae7e
Harivansh <jazzz1815@gmail.com>
2026-08-31 11:28:49 +0530

    feat: add Attribute schema and update package.json scripts

---
+++ apps/server/src/attributes/Attribute.schema.ts
@@ -0,0 +1,28 @@
+import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+import { pgEnum, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { pgTable } from 'drizzle-orm/pg-core';
+
+const AttrubuteType = pgEnum('attribute_type', [
+  'INCOME',
+  'PETROL',
+  'FOOD',
+  'VEGETABLES',
+  'OTHERS',
+]);
+
+const Attribute = pgTable('attributes', {
+  id: uuid('uuid').primaryKey(),
+  name: varchar('name').notNull(),
+
+  type: AttrubuteType('type').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export { Attribute, AttrubuteType };
+
+export type Attribute = InferInsertModel<typeof Attribute>;
+export type AttributeInsert = InferSelectModel<typeof Attribute>;
+
+// InferSelectModel — jab DB se DATA NIKAAL rahe ho
+// InferInsertModel — jab DB se DATA INSERT KAR rahe ho
